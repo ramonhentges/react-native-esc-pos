a6b08b9575fc3771f0b3cb0d427fa899051b213a
Ramon Hentges <r.hentges@hotmail.com>
2021-10-04 10:46:08 -0300

    Update .d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,29 +1,33 @@
 import { EmitterSubscription } from "react-native";
+export default EscPos;
+export namespace EscPos {
+    function addListener(eventName: 'bluetoothStateChanged' | 'bluetoothDeviceFound', callback: (event: eventObject)=>void): EmitterSubscription;
+    function connect(address: string, port?: string): Promise<void>;
+    function scanDevices(): void;
+    function stopScan(): void;
+    function setConfig(props: setConfigProps): void;
+    function setPrintingSize(size: string): void;
+    function setTextDensity(density: number): void;
+    function printSample(): Promise<void>;
+    function cutPart(): Promise<void>;
+    function beep(): Promise<void>;
+    function disconnect(): Promise<void>;
+    function cutFull(): Promise<void>;
+    function kickCashDrawerPin2(): Promise<void>;
+    function kickCashDrawerPin5(): Promise<void>;
+    function printImage(base64: string): Promise<void>;
+    function printImageWithOffset(base64: string, offset: number): Promise<void>;
+    function printDesign(design: string): Promise<void>;
+    function printQRCode(text: string, size: number): Promise<void>;
+    function printBarcode(code: string, type: BARCODES, width: number, height: number, font: FONTS, fontPosition: FONTPOSITION): Promise<void>;
+    const PRINTING_SIZE_58_MM: string;
+    const PRINTING_SIZE_78_MM: string;
+    const PRINTING_SIZE_80_MM: string;
+    const BLUETOOTH_DEVICE_FOUND: string;
+    const BLUETOOTH_CONNECTED: string;
+}
 
-export function addListener(eventName: 'bluetoothStateChanged' | 'bluetoothDeviceFound', callback: (event: eventObject)=>void): EmitterSubscription;
-export function connect(address: string, port?: string): Promise<void>;
-export function scanDevices(): void;
-export function stopScan(): void;
-export function setConfig(props: setConfigProps): void;
-export function setPrintingSize(size: string): void;
-export function setTextDensity(density: number): void;
-export function printSample(): Promise<void>;
-export function cutPart(): Promise<void>;
-export function beep(): Promise<void>;
-export function disconnect(): Promise<void>;
-export function cutFull(): Promise<void>;
-export function kickCashDrawerPin2(): Promise<void>;
-export function kickCashDrawerPin5(): Promise<void>;
-export function printImage(base64: string): Promise<void>;
-export function printImageWithOffset(base64: string, offset: number): Promise<void>;
-export function printDesign(design: string): Promise<void>;
-export function printQRCode(text: string, size: number): Promise<void>;
-export function printBarcode(code: string, type: BARCODES, width: number, height: number, font: FONTS, fontPosition: FONTPOSITION): Promise<void>;
-export const PRINTING_SIZE_58_MM: string;
-export const PRINTING_SIZE_78_MM: string;
-export const PRINTING_SIZE_80_MM: string;
-export const BLUETOOTH_DEVICE_FOUND: string;
-export const BLUETOOTH_CONNECTED: string;
+export const LayoutBuilder: any;
 
 export enum FONTPOSITION {
     'none' = 0,
